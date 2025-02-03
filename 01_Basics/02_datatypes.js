ef58af4826c0e37f87cddef812f3fb3741326e6f
Shweta Mandavkar <shwetamandavkar19@gmail.com>
2025-02-03 11:20:07 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+2);//we are using node js then its provide error
+
+console.log("hello shweta");
+//when writting a code , code readiablity is important
+
+/*Number,bigInt,string,boolean,null,undefined,symbol(unique) , object, typeof*/
+
+
+let age=19;
+let name="shweta";
+let isloggedIn=true;
+let state;
+let role=null;
+
+console.table([typeof age,typeof name,typeof isloggedIn,typeof state, typeof role]);
+
+/* typeof undefined is undefined 
+  typeod null is object
+
+*/

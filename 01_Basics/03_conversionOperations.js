10e0464da9ef98af3f64f7f0a1b9bd9afe9efe0b
Shweta Mandavkar <shwetamandavkar19@gmail.com>
2025-02-03 11:49:01 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score="33";
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let isValueNumber=Number(score);
+console.log(typeof isValueNumber);
+
+/* "33"=> 33
+    "33abc"=>NAN
+    true=1, false=0;
+
+*/
+
+
+
+//boolean conversion
+
+let isLogged="shweta";
+
+let booleanisLogged=Boolean(isLogged);
+console.log(typeof booleanisLogged);
+
+/* 1=> true , 0=?false , ""=>fasle , "shweta"=> true*/
+
+let someNumber=90;
+let convertNumber=String(someNumber);
+
+console.log(convertNumber);
+console.log(typeof convertNumber);
+

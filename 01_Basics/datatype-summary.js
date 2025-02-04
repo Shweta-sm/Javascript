86959fea6122982d59dd00e647ad5353957aade0
Shweta Mandavkar <shwetamandavkar19@gmail.com>
2025-02-04 05:48:32 +0000

    datatype summary

---
+++ 01_Basics/datatype-summary.js
@@ -0,0 +1,36 @@
+
+//primitive datatype: value type:7 types:String, Number,Boolean, Null,undefined,symbol(make the value unique),BigInt
+
+//Non Primitive :refrennce type: Array,objects,Functions
+
+
+let value=12;
+
+let anotherValue=10.9;
+
+
+let islogged=true;
+
+let result=null;
+let demo;
+
+let id=Symbol('123');
+let anotherid=Symbol('123');
+console.log(id===anotherid);
+const BigNumber=897354365425472n;
+
+
+//Array, objects, functions (type of this dt is object )
+
+const Hero=["Mango","Apple","Grapes"];
+
+let myObj={
+    name:"shweta",
+    age:12,
+}
+
+const myFuntion=function(){
+    console.log("Hello Function");
+    
+}
+

df7b41ac926e674b39d9ed6b8d02bf085678ae66
Shweta Mandavkar <shwetamandavkar19@gmail.com>
2025-02-04 05:26:25 +0000

    Data type conparison

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score="33";
 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let isValueNumber=Number(score);
-console.log(typeof isValueNumber);
+// console.log(typeof isValueNumber);
 
 /* "33"=> 33
     "33abc"=>NAN
@@ -20,15 +20,57 @@ console.log(typeof isValueNumber);
 let isLogged="shweta";
 
 let booleanisLogged=Boolean(isLogged);
-console.log(typeof booleanisLogged);
+// console.log(typeof booleanisLogged);
 
 /* 1=> true , 0=?false , ""=>fasle , "shweta"=> true*/
 
 let someNumber=90;
 let convertNumber=String(someNumber);
 
-console.log(convertNumber);
-console.log(typeof convertNumber);
+// console.log(convertNumber);
+// console.log(typeof convertNumber);
 
 //****************Operations ***************/
+
+let value=3;
+let negValue=-value;
+
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1="hello";
+let str2=" shweta";
+
+let str3=str1+str2;
+// console.log(str3);
+
+
+// 
+
+// console.log("2"+2);//22
+// console.log(2+"2");
+// console.log("2"+"2");
+// console.log(2+2+"2");//42
+// console.log("2"+2+4);//224
+
+//Prefix
+
+let x=8;
+let y=++x;
+console.table([x,y]);
+
+
+//postfix
+let x2=7;
+let y2=x2++
+
+console.table([x2,y2])
 
---
+++ 01_Basics/04_Comparison_dt.js
@@ -0,0 +1,32 @@
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2<=1);
+// console.log(2>=1);
+
+
+//avoid this type of comparison
+
+// console.log("2"<1);
+// console.log(2<"1");
+
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+// console.log(undefined<0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+
+
+//strict equality check  : check data types nad value also
+
+
+console.log("2"===1);
+console.log(1===1);
+console.log("tom"==="tom");
+
+
+

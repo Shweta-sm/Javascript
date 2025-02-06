130d2f9621879036f5f61930a2d52141a70db4d3
Shweta Mandavkar <shwetamandavkar19@gmail.com>
2025-02-06 05:12:44 +0000

    Number_Maths

---
+++ 01_Basics/06_number_maths.js
@@ -0,0 +1,65 @@
+//Number n maths in js
+
+const score=400;
+// console.log(score);
+
+const balance= new Number(100);
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+//precision
+
+const othernumber=12.6780;
+// console.log(othernumber.toPrecision(3));
+
+// const othernumber2=124.6780;
+// console.log(othernumber2.toPrecision(3));
+
+// const othernumber3=1224.6780;
+// console.log(othernumber3.toPrecision(3));
+
+//toLocaltestring
+
+const hundred=1000000;
+
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++
+
+//Maths
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(3,7,1,2,5));
+// console.log(Math.max(5,9,10,12,4));
+//console.log(Math.random()); //value lies between 0-1
+
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+//for dice game
+
+const min=10;
+const max=20;
+
+Math.random()*(min-max+1)+min 
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+

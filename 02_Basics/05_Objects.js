381c5ef6ce8865bb72d3ba183c4d5174f28f0cce
Shweta Mandavkar <shwetamandavkar19@gmail.com>
2025-02-07 05:47:58 +0000

    object part3

---
+++ 02_Basics/05_Objects.js
@@ -0,0 +1,28 @@
+//Objects de-structure
+
+
+const user={
+
+    id:12,
+    email:"shweta@gmail.com",
+    company:"shweta"
+
+}
+
+const{company:com}=user
+console.log(com);
+
+
+//api basic declaration
+
+// {
+//     "id":"12",
+//     "name":"shweta",
+//     "email":"shweta@gmail.com"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]

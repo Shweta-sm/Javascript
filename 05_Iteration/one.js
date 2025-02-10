57c198c89e6326894d2b5e8ae0834a3e07ba05e9
Shweta Mandavkar <shwetamandavkar19@gmail.com>
2025-02-10 06:51:15 +0000

    For Loop

---
+++ 05_Iteration/one.js
@@ -0,0 +1,38 @@
+//for loop
+
+for (let i=0;i<=10;i++){
+    const element =i;
+    //console.log(element);
+    }
+
+
+    for (let i=0;i<=10;i++){
+        //console.log(`Outer loop: ${i}`);
+        
+        for (let j = 0; j <=10; j++) {
+          // console.log(`Inner Loop: ${j}`);
+    }}
+    
+//Array iteration
+
+let myArray=["Mango","Apple","Cherry"]
+
+for (let k = 0; k < myArray.length; k++) {
+    const element = myArray[k];
+    console.log(element);
+    
+}
+
+
+//break,contiue
+
+for (let i=1;i<=10;i++){
+    if (i==5) {
+        console.log("5 Detected");
+        continue
+        
+    }
+    console.log(i);
+
+}
+

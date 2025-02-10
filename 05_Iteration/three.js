c85c95dfce2eb24174f5f3ccdc457b40dccbb9a9
Shweta Mandavkar <shwetamandavkar19@gmail.com>
2025-02-10 09:16:54 +0000

    For In loop

---
+++ 05_Iteration/three.js
@@ -0,0 +1,24 @@
+const myObject={
+   js:'JavaScript',
+   rb:'Ruby',
+   cpp:'C++',
+   swift:'Swift by app'
+}
+
+//for in loop
+
+for (const key in myObject) {
+  //console.log(`${key} shortcut is for ${myObject[key]}`);
+  
+}
+
+//Array
+
+const programming=["js","py","java","cpp"]
+
+for (const key in programming) {
+console.log(programming[key]);
+
+}
+
+//on maps for in loop not work
